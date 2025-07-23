5f69821a50259c87a79edbf751cef41528daa374
quangngthanh <nguyenthanhquanga7@gmail.com>
2025-07-23 09:29:43 +0700

    update pattern

--- dist/file-watcher.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class FileWatcher {
-    private watcher?;
-    private watchedPaths;
-    watch(projectRoot: string, onChange: (changedFiles: string[]) => void): Promise<void>;
-    stopWatching(): Promise<void>;
-    isWatching(projectRoot: string): boolean;
-    getWatchedPaths(): string[];
-}
-//# sourceMappingURL=file-watcher.d.ts.map
